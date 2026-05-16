4086aaa0e2f45daae15fe8854bb61acbb254c845
Thakrar Jeet <jd.thakrar05@gmail.com>
2026-05-16 19:27:19 +0530

    75. Sort Colors.

---
+++ Medium/75.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void swap(int& n, int& m) {
+        int temp = n;
+        n = m;
+        m = temp;
+    }
+    void sortColors(vector<int>& nums) {
+        int n = nums.size() - 1, left = 0, right = n, mid = 0;
+
+        while (mid <= right) {
+
+            if (nums[mid] == 0) {
+                swap(nums[mid], nums[left]);
+                left++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            }
+            else {
+                swap(nums[mid], nums[right]);
+                right--;
+            }
+        }
+    }
+};
