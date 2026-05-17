597a6f1fd550978767d836232ed714f95f9db1dd
Thakrar Jeet <jd.thakrar05@gmail.com>
2026-05-17 10:54:41 +0530

    14. Longest Common Prefix.

---
+++ Easy/14.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        int n = strs.size();
+        int m = strs[0].size(), count = 0;
+        string res="";
+
+        if(n==1){
+            return strs[0];
+        }
+        for (int i = 0; i < m; i++) {
+            count = 0;
+            for (int j = 0; j < n-1; j++) {
+                
+                if (strs[j][i] == strs[j + 1][i]) {
+                    count++;
+                }else{
+                    return res;
+                }
+
+                if (count == n-1) {
+                    res += strs[j][i];
+                }
+            }
+            
+        }
+        return res;
+    }
+};
