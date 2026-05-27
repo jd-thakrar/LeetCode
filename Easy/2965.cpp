43af5d24d90ab64a5bc34c71979175907512caca
Thakrar Jeet <jd.thakrar05@gmail.com>
2026-05-27 10:42:35 +0530

    2965. Using Set Method

--- Easy/2965.cpp
+++ Easy/2965.cpp
@@ -1,3 +1,5 @@
+
+//Using the Unorderd Map method
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
@@ -24,6 +26,34 @@ public:
             }
         }
 
+        return ans;
+    }
+};
+
+
+
+
+//Using the Set method
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size(), expSum = 0, actSum = 0, a;
+        vector<int> ans;
+        unordered_set<int> s;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                actSum += grid[i][j];
+                if (s.find(grid[i][j]) != s.end()) {
+                    a = grid[i][j];
+                    ans.push_back(a);
+                }
+                s.insert(grid[i][j]);
+            }
+        }
+        expSum = (n * n) * (n * n + 1) / 2;
+        int b = expSum - actSum + a;
+        ans.push_back(b);
         return ans;
     }
 };
