bf6ee1b45c256b97e7215c3d5d6b0f9bb7c816be
Thakrar Jeet <jd.thakrar05@gmail.com>
2026-05-27 17:33:52 +0530

    287. Using Linked list

--- Medium/287.cpp
+++ Medium/287.cpp
@@ -1,3 +1,4 @@
+//Using Unordered Set
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
@@ -14,4 +15,27 @@ public:
     }
 };
 
+
+//Using Slow Fast Pointers
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0], fast= nums[0];
+
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        
+        slow = nums[0];
+
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+};
 //https://leetcode.com/problems/find-the-duplicate-number/solutions/8297053/287-find-the-duplicate-number-using-hash-ijyr
+//https://leetcode.com/problems/find-the-duplicate-number/solutions/8297095/287-find-the-duplicate-number-using-link-hp5r
