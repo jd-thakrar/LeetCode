fe18730732295c0fc5d0cbfacbfdbf2320086015
Thakrar Jeet <jd.thakrar05@gmail.com>
2026-06-13 01:15:05 +0530

    503. Next Greater element II.

---
+++ Medium/503.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
+        int n = nums.size();
+        vector<int> ans(n, -1);
+        for (int i = 0; i < n*2 ; i++) {
+            while(!st.empty() && nums[i%n] > nums[st.top()] ){
+                ans[st.top()] = nums[i%n];
+                st.pop();
+            }
+            st.push(i%n);
+        }
+        return ans;
+    }
+};
