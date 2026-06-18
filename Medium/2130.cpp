2bb306d496229e7013356337ed8f7c7231a3f182
Thakrar Jeet <jd.thakrar05@gmail.com>
2026-06-18 12:45:01 +0530

    2130. Maximum Twin Sum of a Linked List. another approach

--- Medium/2130.cpp
+++ Medium/2130.cpp
@@ -28,3 +28,41 @@ public:
      return ans;
     }
 };
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int pairSum(ListNode* head) {
+        ListNode *slow = head,* fast = head;
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode *cur=slow, *prev=nullptr, *nex;
+        while(cur) {
+            nex=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=nex;
+        }
+        int ans =0;
+        while(prev){
+            ans = max(ans, prev->val + head->val);
+            prev = prev->next;
+            head = head->next;
+        }
+        return ans;
+    }
+};
