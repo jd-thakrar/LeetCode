383bb877da3aafb5566d2fded81a40aebddd1ec4
Thakrar Jeet <jd.thakrar05@gmail.com>
2026-07-05 04:55:39 +0530

    120. Triangle

---
+++ Medium/120.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+        int n = triangle.size();
+        
+        vector<vector<int>> dp(n, vector<int>(n,0));
+        
+        for( int i = 0; i<n;i ++){
+            for(int j = 0; j<=i;j++){
+                if(i==0 && j==0){
+                    dp[i][j] = triangle[i][j];
+                }
+                
+                else if(j==0){
+                    dp[i][j]= dp[i-1][j]+triangle[i][j];
+                }
+                else if(j==i){
+                    dp[i][j]= dp[i-1][j-1]+triangle[i][j];
+               }
+                else{
+                    dp[i][j] = triangle[i][j] + min(dp[i-1][j-1], dp[i-1][j]);
+                }
+            }
+            
+        }
+        
+        int minpath = dp[n-1][0];
+        for(int i = 1; i<n;i++){
+            minpath = min(minpath, dp[n-1][i]);
+        }
+        return minpath;
+    }
+};
