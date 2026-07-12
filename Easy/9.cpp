b60c15757bb771cfd7e1d9ba800cc14ab5b1e53a
Thakrar Jeet <jd.thakrar05@gmail.com>
2026-07-12 23:35:14 +0530

    9. Palindrome Number

---
+++ Easy/9.cpp
@@ -0,0 +1,15 @@
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string a = to_string(x);
+        int l = 0, r = a.size() - 1;
+        while(l<=r){
+            if(a[l] != a[r])
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
