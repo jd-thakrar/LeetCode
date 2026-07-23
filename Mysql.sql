a473b9c0c22568e7cc065dd2ed4280f84c00fc98
Thakrar Jeet <jd.thakrar05@gmail.com>
2026-07-23 23:51:46 +0530

    1148. Article Views I

--- Mysql.sql
+++ Mysql.sql
@@ -5,3 +5,9 @@ select p.firstName, p.lastName, a.city, a.state
 from Person p left join Address a
 on p.personId = a.personId;
 
+-- 1148. Article Views I
+select author_id as id 
+from views 
+where author_id = viewer_id 
+Group by author_id 
+Order by author_id;
